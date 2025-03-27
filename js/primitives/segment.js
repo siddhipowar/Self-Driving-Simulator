b6772927e10ad68f74d39f63d451ed52911b0039
siddhipowar <siddhi.powar2210@gmail.com>
2025-03-27 16:03:21 -0700

    Added graph functionalities
    Added fuctionalities such as pan, zoom in, zoom out, save and delete the graph.

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -13,12 +13,14 @@ class Segment {
     }
 
 
-    draw(ctx, width = 2, color = "black") {
+    draw(ctx, { width = 2, color = "black", dash = [] } = {} ) {
         ctx.beginPath();
         ctx.linewidth = width;
         ctx.stokestyle = color;
+        ctx.setLineDash(dash);
         ctx.moveTo(this.p1.x, this.p1.y);
         ctx.lineTo(this.p2.x, this.p2.y);
         ctx.stroke();
+        ctx.setLineDash([]);
     }
 }
