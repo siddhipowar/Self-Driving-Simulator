9ec5437c6c2976e37ddcab88a3a379465ac1469a
siddhipowar <siddhi.powar2210@gmail.com>
2025-03-26 22:00:14 -0700

    Remove segment
    Remove segments feature added.

--- js/math/graph.js
+++ js/math/graph.js
@@ -24,6 +24,22 @@ class Graph {
         this.segments.push(seg);
     }
 
+    containsSegment(seg) {
+        return this.segments.find((s) => s.equals(seg));
+    }
+
+    tryAddSegment(seg) {
+        if(!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
+    removeSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
     draw(ctx) {
         for (const seg of this.segments) {
             seg.draw(ctx);
